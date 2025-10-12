fea91e506bc516257fb8926f39cf6a7d2159ac3c
joenju <dmswn123486@gmail.com>
2025-10-12 23:57:47 +0900

    added transfer

--- contracts/MyToken.sol
+++ contracts/MyToken.sol
@@ -1,23 +1,28 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.28;
+pragma solidity ^0.8.20;
 
 contract MyToken {
-
     string public name;
     string public symbol;
-    uint8 public decimals; // uint8 --> 8bit unsigned int, uint16, ... , uint256
+    uint8 public decimals; // uint8 --> 8 bit unsigned int, uint16, ..., uint256
+    uint256 public totalSupply;
+    mapping(address => uint256) public balanceOf;
 
-    constructor(
-        string memory _name,
-        string memory _symbol,
-        uint8 _decimal
-        // uint256 _amount
-    ) {
+    constructor(string memory _name, string memory _symbol, uint8 _decimal, uint256 _amount) {
         name = _name;
         symbol = _symbol;
         decimals = _decimal;
-        // The amount is multiplied by 10 to the power of decimals
-        // to create the correct total supply based on the token's decimal places.
-        // _mint(_amount * 10 ** uint256(decimals), msg.sender);
+        _mint(_amount * 10 ** uint256(decimals), msg.sender);
+    }
+
+    function _mint(uint256 amount, address owner) internal {
+        totalSupply += amount;
+        balanceOf[owner] += amount;
+    }
+
+    function transfer(uint256 amount, address to) external {
+        require(balanceOf[msg.sender] >= amount, "insufficient balance");
+        balanceOf[msg.sender] -= amount;
+        balanceOf[to] += amount;
     }
 }
